8275c646e8c20038cc13e161794d6ad8355781f4
dhanyaravi30 <dhanyafab@gmail.com>
2025-01-31 21:00:45 +0530

    Done

---
+++ photo-gallery/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import ImageGrid from './components/ImageGrid';
+
+function App() {
+  const [images, setImages] = useState([]);
+
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">📸 Photo Gallery</h1>
+      <SearchBar setImages={setImages} />
+      <ImageGrid images={images} />
+    </div>
+  );
+}
+
+export default App;
